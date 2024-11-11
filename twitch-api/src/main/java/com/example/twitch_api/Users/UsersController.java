2226639b35b55bc74633688ce42128bf4c5379c9
Timothy Choi <tchoi720@gmail.com>
2024-11-10 23:31:20 -0800

    adding create and delete user endpoints. stopping there for now

--- twitch-api/src/main/java/com/example/twitch_api/Users/UsersController.java
+++ twitch-api/src/main/java/com/example/twitch_api/Users/UsersController.java
@@ -1,12 +1,16 @@
 package com.example.twitch_api.Users;
 
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,4 +46,41 @@ public class UsersController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(curr);
     }
+
+    @PostMapping("/")
+    public ResponseEntity<Users> createUser(@RequestBody Map<String, Object> reqBody) {
+        try {
+            Users currUser = _userRepository.findByTwitchUsername(reqBody.get("twitchUsername").toString());
+
+            if (currUser != null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+
+            Users incomingUser = new Users(reqBody.get("username").toString(), reqBody.get("description").toString(), reqBody.get("twitchUsername").toString(), reqBody.get("twitchImageUrl").toString());
+
+            _userRepository.save(incomingUser);
+
+            return ResponseEntity.status(HttpStatus.OK).body(incomingUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @DeleteMapping("/{userId}") 
+    public ResponseEntity deleteUser(@PathVariable UUID userId) {
+        try {
+            Users currUser = _userRepository.findByUserId(userId);
+
+            if (currUser == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+
+            _userRepository.delete(currUser);
+
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+    
 }
