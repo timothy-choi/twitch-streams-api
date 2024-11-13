0a1d3c5a35336e5267a58b6dfd8ffb889940e31f
Timothy Choi <tchoi720@gmail.com>
2024-11-13 14:46:48 -0800

    fixed some config issues to make the api run

--- demo2/src/main/java/comm/example/demo2/Demo2Application.java
+++ demo2/src/main/java/comm/example/demo2/Demo2Application.java
@@ -3,10 +3,10 @@ package comm.example.demo2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "comm.exmaple.demo2")
-@EnableJpaRepositories(basePackages = "comm.exmaple.demo2")
+@SpringBootApplication(scanBasePackages = "comm.exmaple.demo2", exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 @EntityScan(basePackages = "comm.exmaple.demo2")
 public class Demo2Application {
 	public static void main(String[] args) {
